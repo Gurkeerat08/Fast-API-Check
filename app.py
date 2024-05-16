2fd022bdb7c64e46288609303f8b2898b35b273c
Gurkeerat Singh Sondhi <152252215+Gurkeerat08@users.noreply.github.com>
2024-05-16 06:53:46 +0000

    json

--- app.py
+++ app.py
@@ -3,9 +3,14 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    # Define your data here as a dictionary
+    data = {
+        "message": "Hello, World!"
+    }
+    # Use jsonify to return the data in JSON format
+    return jsonify(data)
