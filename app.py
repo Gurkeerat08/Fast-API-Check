9cbd6215018bc7710c2a40e2dbd7d61f71b921aa
Gurkeerat Singh Sondhi <152252215+Gurkeerat08@users.noreply.github.com>
2024-05-16 08:39:24 +0000

    added API call to index.html

--- app.py
+++ app.py
@@ -1,22 +1,11 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
+from fastapi import FastAPI
+from starlette.responses import FileResponse 
+app = FastAPI()
 
-from flask import Flask, jsonify
-app = Flask(__name__)
+@app.get("/ui")
+async def read_index():
+    return FileResponse('static/index.html')
 
-@app.route("/")
-def hello():
-    # Define your data here as a dictionary
-    data = {
-        "message": "Hello, World!"
-    }
-    print("Hello")
-    # Use jsonify to return the data in JSON format
-    return jsonify(data)
-
-@app.route("/test")
-def ui():
-    # Define your data here as a dictionary
-    return app.send_static_file("index.html")
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
